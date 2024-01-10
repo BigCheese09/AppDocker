990cafee033be63546d6df4f9dde1ca0d4e17cd5
BigCheese09 <79065309+BigCheese09@users.noreply.github.com>
2024-01-09 22:12:18 -0500

    Update deportes.php

--- secciones/deportes.php
+++ secciones/deportes.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $deportes = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Jorge Fossati: "Quiero que el pueblo peruano, con su selección, vuelva a ser feliz”",
+"autor" => "El Peruano Depor",
+"resumen" => "El técnico Jorge Fossati, quien llegó este lunes a Lima para ser presentado, de manera oficial el miércoles 10 de enero como seleccionador peruano, dijo que se siente con "muchas ganas" y "mucha fe" para comenzar su nueva etapa al mando de la Bicolor, ya que se desea que la afición nacional sea nuevamente feliz con la selección.",
 ];
 ?>
