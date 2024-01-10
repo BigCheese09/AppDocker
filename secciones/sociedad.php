72a46f2ae2846784280ecb990f0ec5493b608ab7
BigCheese09 <79065309+BigCheese09@users.noreply.github.com>
2024-01-09 22:15:49 -0500

    Update sociedad.php

--- secciones/sociedad.php
+++ secciones/sociedad.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "A nuestra sociedad le falta márketing",
+"autor" => "Rolando Arellano C.",
+"resumen" => "En momentos de poco crecimiento y de graves problemas, la sociedad peruana podría avanzar mejor si sus empresas e instituciones tuvieran como prioridad la satisfacción de los ciudadanos a los que sirven. Prioridad de ponerse al servicio de su ‘market’ (‘mercado’), de donde sale el nombre de la muy mal conocida disciplina del márketing.",
 ];
 ?>
