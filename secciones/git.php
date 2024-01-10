bf050ce13ac43ab223faff2d52f34eebbff35ebc
BigCheese09 <79065309+BigCheese09@users.noreply.github.com>
2024-01-09 22:17:22 -0500

    Update git.php

--- secciones/git.php
+++ secciones/git.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Fundador de GIT: “Es una época con demasiado WhatsApp y nada de rock and roll”",
+"autor" => "Ricardo Hinojosa Lizárraga",
+"resumen" => "“Desde que vi por primera vez una batería me enamoré y ya estoy casado hace muchos años con ella”, dice entre risas Willy Iturri, un prócer del rock argentino que supo estar en el momento y el lugar correctos, con el talento preciso para convertirse en el baterista favorito de muchos de los grandes de la historia del rock en su país y Latinoamérica.
+",
 ];
 ?>
