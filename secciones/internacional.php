1edd5cfd3ddde2bfa024ce66b47768e7f78f4f72
BigCheese09 <79065309+BigCheese09@users.noreply.github.com>
2024-01-09 22:27:37 -0500

    Update internacional.php

--- secciones/internacional.php
+++ secciones/internacional.php
@@ -4,7 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "¿En qué consiste el "conflicto armado interno" que Noboa decretó en Ecuador y qué implica?
+"titulo" => "¿En qué consiste el conflicto armado interno que Noboa decretó en Ecuador y qué implica?
 ",
 "autor" => "CNN Español",
 "resumen" => "El presidente de Ecuador, Daniel Noboa, decretó este martes la existencia de un "conflicto armado interno" en el país tras la escalada de violencia que ha azotado al territorio durante las últimas horas. Pero ¿qué es y qué implica exactamente esa medida?",
