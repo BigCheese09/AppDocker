825c170f0e1b1cc6392b6907f3c7dbd86dc8f074
BigCheese09 <79065309+BigCheese09@users.noreply.github.com>
2024-01-09 22:09:50 -0500

    Update ciencia.php

--- secciones/ciencia.php
+++ secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Inteligencia artificial: La tecnología puede hacernos hablar en otro idioma",
+"autor" => "Bruno Ortiz Bisso",
+"resumen" => "Hace unos días compartí, a través de mis redes sociales, un video que sorprendió a algunos de mis contactos porque era totalmente fuera de lo común: aparecía yo haciendo un comentario, de un minuto de duración, hablando en inglés con una pronunciación y fluidez bastante buenas. Lo raro es que, en la vida real, nunca he sido capaz de hacerlo. Todo era producto de la inteligencia artificial.",
 ];
 ?>
