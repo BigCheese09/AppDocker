451b61e7a02d28c0cf6eec50eeaf516d9d24bfa9
BigCheese09 <79065309+BigCheese09@users.noreply.github.com>
2024-01-09 22:10:41 -0500

    Update cultura.php

--- secciones/cultura.php
+++ secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La aldea perdida donde las mujeres se juegan la vida en las montañas y los hombres se quedan en casa cultivando",
+"autor" => "BBC Mundo",
+"resumen" => "La de las pastoras Wakhi es una forma de vida que desaparece. BBC 100 Women visitó su aldea y los campos que les permitieron ganar dinero para cambiar por completo su hogar en las montañas",
 ];
 ?>
