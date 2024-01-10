0c0a45a4dc319caa513e6a953d23ed4f396734ce
BigCheese09 <79065309+BigCheese09@users.noreply.github.com>
2024-01-09 22:07:42 -0500

    Update economia.php

--- secciones/economia.php
+++ secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Banco Mundial estima que economía del Perú crecerá 2.5% este 2024",
+"autor" => "Eva Cruz",
+"resumen" => "De acuerdo al informe “Perspectivas Económicas” del Banco Mundial (BM), la economía peruana registraría un crecimiento de 2.5% en el 2024 y 2.3% el próximo año.",
 ];
 ?>
