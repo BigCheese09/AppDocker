3dae00148b334b6a643c4da40db83b6f2b672fbb
BigCheese09 <79065309+BigCheese09@users.noreply.github.com>
2024-01-09 22:11:29 -0500

    Update gente.php

--- secciones/gente.php
+++ secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Operador de maquinaria a punto de morir tras quedar atrapado en río Cañete",
+"autor" => "Redaccion EC",
+"resumen" => "Un maquinista de una retroexcavadora quedó atrapado durante varios minutos en el río Cañete, al sur de Lima, cuando realizaba trabajos de descolmatación para evitar las inundaciones.",
 ];
 ?>
