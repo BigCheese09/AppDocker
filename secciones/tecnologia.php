e8b2325055dde2524cc3066a947aafacd74c2f4d
BigCheese09 <79065309+BigCheese09@users.noreply.github.com>
2024-01-09 22:08:59 -0500

    Update tecnologia.php

--- secciones/tecnologia.php
+++ secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Microsoft contrata a Sam Altman tras su despido de OpenAI",
+"autor" => "Agencia EFE",
+"resumen" => "El consejero delegado de Microsoft, Satya Nadella, ha anunciado que la tecnológica contratará a Sam Altman para que lidere la creación de un nuevo departamento enfocado en la inteligencia artificial (IA), después de su despido sorpresa de OpenAI el pasado viernes.",
 ];
 ?>
