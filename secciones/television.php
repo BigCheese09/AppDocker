bc28eb8346395da6d1e56838bf25b54c6e94fe00
BigCheese09 <79065309+BigCheese09@users.noreply.github.com>
2024-01-09 22:31:36 -0500

    Update television.php

--- secciones/television.php
+++ secciones/television.php
@@ -6,7 +6,6 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 $television = [
 "titulo" => "Ecuador: encapuchados armados toman un canal de televisión en transmisión en vivo",
 "autor" => "Redacción Gestión",
-"resumen" => "Encapuchados armados ingresaron este martes en el canal de televisión TC, en la ciudad de Guayaquil en el suroeste de Ecuador, y sometieron a su personal durante una transmisión en vivo.
-",
+"resumen" => "Encapuchados armados ingresaron este martes en el canal de televisión TC, en la ciudad de Guayaquil en el suroeste de Ecuador, y sometieron a su personal durante una transmisión en vivo.",
 ];
 ?>
