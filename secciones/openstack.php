5848025f00587d19b104dcd5b9bc2dfe300a32bc
BigCheese09 <79065309+BigCheese09@users.noreply.github.com>
2024-01-09 22:16:45 -0500

    Update openstack.php

--- secciones/openstack.php
+++ secciones/openstack.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openstack = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "VMware celebra su VMworld 2017 con pocas novedades",
+"autor" => "Manuel Navarro",
+"resumen" => "Un nuevo éxito ha sido esta edición europea de VMworld 2017 que se ha celebrado en Barcelona. Una ciudad que gusta a sus principales ejecutivos ya que, a pesar de la situación política incierta, han decidido que el próximo año vuelven a repetir escenario.",
 ];
 ?>
