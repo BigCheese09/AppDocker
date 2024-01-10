2d4b56e98939dfe418e34b8fbfb012da9eaf2199
BigCheese09 <79065309+BigCheese09@users.noreply.github.com>
2024-01-09 22:43:54 -0500

    Update opinion.php

--- secciones/opinion.php
+++ secciones/opinion.php
@@ -4,7 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "Meritocracia : La ciencia y arte de identificar y recompensar a los ‘mejores’",
+"titulo" => "Meritocracia : La ciencia y arte de identificar y recompensar a los mejores",
 "autor" => "Martín Salas",
 "resumen" => "El principio de meritocracia en la gestión de personas radica en reconocer y promover el mérito y -algunos declaran- a los mejores colaboradores. Entonces, que es mérito y quiénes son los mejores en una empresa, y lo más importante, cómo se miden ambos.",
 ];
