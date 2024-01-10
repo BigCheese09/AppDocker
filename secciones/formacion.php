4f3e523f77f70cb6ced4cc25043e191304c3b561
BigCheese09 <79065309+BigCheese09@users.noreply.github.com>
2024-01-09 22:14:00 -0500

    Update formacion.php

--- secciones/formacion.php
+++ secciones/formacion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $formacion = [
-"titulo" => "XXX",
+"titulo" => "Aprenda literatura con Taylor Swift: la cantante conquista las universidades con cursos sobre sus letras",
 "autor" => "Gabinete de comunicación",
-"resumen" => "XXX",
+"resumen" => "Cada vez más facultades, en Estados Unidos como en Europa, imparten clases que relacionan sus canciones con la obra de grandes autores, de Shakespeare a Sylvia Plath",
 ];
 ?>
