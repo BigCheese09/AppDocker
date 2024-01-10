4e4b3b96555a7206ffb8ada4a2970a3428e27ba3
BigCheese09 <79065309+BigCheese09@users.noreply.github.com>
2024-01-09 22:18:28 -0500

    Update openshift.php

--- secciones/openshift.php
+++ secciones/openshift.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Red Hat y Microsoft desarrollan el primer servicio de Red Hat OpenShift gestionado conjuntamente en la nube pública",
+"autor" => "Microsoft Prensa",
+"resumen" => "Microsoft Corp. (Nasdaq “MSFT”) y Red Hat Inc. (NYSE: RHT) han ampliado hoy su alianza para animar a los desarrolladores empresariales a ejecutar aplicaciones basadas en contenedores a través de Microsoft Azure y on-premises. Con esta colaboración, ambas compañías lanzarán el primer servicio OpenShift gestionado conjuntamente, ofertado en la nube pública. Este servicio combina todo el potencial de Red Hat OpenShift, la plataforma empresarial Kubernetes más completa de la industria, y Azure, la nube pública de Microsoft.",
 ];
 ?>
