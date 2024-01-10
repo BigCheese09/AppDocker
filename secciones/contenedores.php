410d45ee7fad5dbda49a0c366e17cb57312bc631
BigCheese09 <79065309+BigCheese09@users.noreply.github.com>
2024-01-09 22:17:58 -0500

    Update contenedores.php

--- secciones/contenedores.php
+++ secciones/contenedores.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "China vende tanto a Rusia que los contenedores se acumulan",
+"autor" => "Mundo",
+"resumen" => "Rusia tiene actualmente un excedente de 150,000 contenedores de envío que los depósitos ferroviarios tienen dificultades para gestionar, lo que refleja un aumento en el flujo de productos chinos hacia el país, pero mucho menor hacia el exterior.",
 ];
 ?>
