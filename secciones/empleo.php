9e5d4df1858f214ab491a48bf7fe69fee98bae35
BigCheese09 <79065309+BigCheese09@users.noreply.github.com>
2024-01-09 22:14:50 -0500

    Update empleo.php

--- secciones/empleo.php
+++ secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "El 18% de las empresas reducirá su personal en el verano 2024",
+"autor" => "Naruto Obeso",
+"resumen" => "La expectativa neta de empleo para el primer trimestre del 2024 es del 23%, lo que representa una reducción del 10% respecto al mismo periodo del 2023 (33%), según la última Encuesta de Empleo de ManpowerGroup Perú. Además, en relación con los últimos tres meses, la caída llega hasta el 11%.",
 ];
 ?>
