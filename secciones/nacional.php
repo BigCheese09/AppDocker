4fd5d04c0dd0e5daf20a9fe3c75e4e2270a76164
BigCheese09 <79065309+BigCheese09@users.noreply.github.com>
2024-01-09 22:07:02 -0500

    Update nacional.php

--- secciones/nacional.php
+++ secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Oleajes anómalos en Perú: playas y puertos del litoral peruano se habilitan tras estar cerrados por más de una semana",
+"autor" => "Laura Vásquez Benavente",
+"resumen" => "Una serie de fuertes oleajes anómalos a lo largo de la costa peruana siguen presentándose. La Marina de Guerra indicó que la alerta se mantendrá hasta el día de hoy. Mientras tanto, 68 puertos aún siguen cerrados",
 ];
 ?>
